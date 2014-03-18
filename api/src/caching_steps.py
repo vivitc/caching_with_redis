5d456db1c8d34951c631a53a10a4655984992f73
vivitc <vivitc@gmail.com>
2014-03-18 16:41:03 -0400

    Adding md5 encoding for script steps.

--- api/src/caching_steps.py
+++ api/src/caching_steps.py
@@ -1,4 +1,5 @@
 import redis
+import hashlib
 
 
 class StepsCache:
@@ -9,7 +10,7 @@ class StepsCache:
     def is_value_in_set(self, client, set_name, value):
         return client.sismember(set_name, value)
 
-    def add_script_data(self, client):
+    def add_script_steps_data(self, client):
         """"
         For the example we have choose set as main value structure.
         Set allows to store unique keys
@@ -17,7 +18,12 @@ class StepsCache:
         data = self.get_steps_information()
         for key in data.iterkeys():
             for value in data[key]:
-                client.sadd(key, value)
+                client.sadd(key, self.encode(value))
 
     def get_steps_information(self):
         return {}
+
+    def encode(self, text):
+        md5 = hashlib.md5()
+        md5.update(text)
+        return md5.digest()
--- api/test/test_steps_cache.py
+++ api/test/test_steps_cache.py
@@ -1,7 +1,20 @@
 from unittest import TestCase
-from mock import patch, Mock
+from mock import patch, Mock, MagicMock
 from mockredis import mock_strict_redis_client
 from src.caching_steps import StepsCache
+
+
+def md5_mock():
+    md5 = Mock()
+    md5.update = Mock()
+    md5.digest = Mock(return_value='encrypted_value')
+    return md5
+
+def encode_mock(encode_values):
+    def side_effect(args):
+        return encode_values[args]
+    mock = MagicMock(side_effect=side_effect)
+    return mock
 
 
 class TestStepsCache(TestCase):
@@ -9,14 +22,25 @@ class TestStepsCache(TestCase):
     @patch('redis.StrictRedis', mock_strict_redis_client)
     def test_that_data_from_api_is_stored(self):
         cache = StepsCache()
+
         key_name = "script 1"
         first_value = "step 1 1"
         second_value = "step 1 2"
         data = {key_name: [first_value, second_value]}
+        encoded_data = {first_value: "encrypted_first_value", second_value: "encrypted_second_value"}
         cache.get_steps_information = Mock(return_value=data)
+        cache.encode = encode_mock(encoded_data)
 
         client = cache.get_redis_client()
-        cache.add_script_data(client)
+        cache.add_script_steps_data(client)
 
-        self.assertTrue(client.sismember(key_name, first_value))
-        self.assertTrue(client.sismember(key_name, second_value))
+        self.assertTrue(client.sismember(key_name, "encrypted_first_value"))
+        self.assertTrue(client.sismember(key_name, "encrypted_second_value"))
+
+
+    @patch('hashlib.md5', md5_mock)
+    def test_that_encode_text_using_md5(self):
+        cache = StepsCache()
+        text = "important text"
+        encoded_text = cache.encode(text)
+        self.assertEquals(encoded_text, 'encrypted_value')
